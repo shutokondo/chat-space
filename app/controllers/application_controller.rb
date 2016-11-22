0720a23a0501ea41e413a6cf25692634562950c3
shutokondo <tech-camp@tech-camp-2-007-no-MacBook-Air.local>
2016-11-22 23:35:39 +0900

    add name to devise strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameter, if: :devise_controller?
+
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
