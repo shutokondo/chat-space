bacad985ffac7806a220ac57b20bc731941b3b90
shutokondo <tech-camp@tech-camp-2-007-no-MacBook-Air.local>
2016-11-26 09:08:52 +0900

    designate path after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameter
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
