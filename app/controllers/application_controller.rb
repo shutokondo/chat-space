550f3cfde7092ace5b52330d89be620f68e881a7
shutokondo <tech-camp@tech-camp-2-007-no-MacBook-Air.local>
2016-12-03 22:27:03 +0900

    add scope

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameter, if: :devise_controller?
 
+  protected
+
   def configure_permitted_parameter
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
